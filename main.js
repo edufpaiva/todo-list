912e1d15d68d2965056c56005007f9b7dd8f33ff
edu paiva <edufpaiva33@gmail.com>
2020-05-27 10:24:11 -0400

    click to expand base layout

--- main.js
+++ main.js
@@ -7,3 +7,24 @@ function download(content, fileName, contentType) {
     a.download = fileName;
     a.click();
 }
+
+function listeners(){
+
+    
+
+    $(".item").click(function (e) { 
+        e.preventDefault();
+        console.log(e);
+        let id = e.target.id
+        
+        id = id.match(/item-[0-9]+/i)[0]
+        
+
+        console.log(id + "-body");
+        
+        if (document.getElementById(id + "-body").style.height == "auto") document.getElementById(id + "-body").style.height = "27px"
+        else document.getElementById(id + "-body").style.height = "auto"
+
+        
+    });
+}
